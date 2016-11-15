83f899b788a0410215b1e492d41169a0cfd403bf
Sara Jensen <sara@>
2016-11-14 17:07:54 -0800

    add logs and debuggers.

--- app/src/main/java/com/example/guest/myrestaurants2/RestaurantsActivity2.java
+++ app/src/main/java/com/example/guest/myrestaurants2/RestaurantsActivity2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,6 +19,7 @@ public class RestaurantsActivity2 extends AppCompatActivity {
     @Bind(R.id.locationTextView) TextView mLocationTextView;
     @Bind(R.id.listView) ListView mListView;
     private String[] restaurants = new String[] {"HUB", "Garden Bar", "Reel M Inn", "People's Pig", "Rabbits Cafe", "Bar Bar", "Slide Inn", "Nightlight", "The Florida Room", "Fresh Roll", "Eat", "Cruz Room", "Radio Room", "Santeria", "Sauce Box", "Shut Up and Eat", "A Real Taste of India (Foodcart)"};
+    public static final String TAG = RestaurantsActivity2.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,7 @@ public class RestaurantsActivity2 extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String restaurant = ((TextView)view).getText().toString();
                 Toast.makeText(RestaurantsActivity2.this, restaurant, Toast.LENGTH_LONG).show();
+                Log.v(TAG, "In the onItemClickListener");
             }
         });
 
@@ -43,5 +46,6 @@ public class RestaurantsActivity2 extends AppCompatActivity {
 //        and the getStringExtra() method to pull out the location value based using the string key we provided:
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
+        Log.d(TAG, "In the onCreate method");
     }
 }
