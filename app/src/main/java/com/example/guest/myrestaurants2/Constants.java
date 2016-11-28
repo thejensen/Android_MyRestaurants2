d7bb04bc15271f0f81fe9240c224695a5563fcca
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-27 21:53:41 -0800

    add yelp api credentials to gradle.properties, add gradle.properties to .gitignore, initialize string constants in Constants.java class, include credentials in BuildConfig file in build.gradle.

---
+++ app/src/main/java/com/example/guest/myrestaurants2/Constants.java
@@ -0,0 +1,12 @@
+package com.example.guest.myrestaurants2;
+
+/**
+ * Created by jensese on 11/27/16.
+ */
+
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
