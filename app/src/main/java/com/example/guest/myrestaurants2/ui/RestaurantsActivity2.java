ec49966b66684dde82e5024c128eacf434fc0d58
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-29 00:24:15 -0800

    refactor, remove old list views of restaurants from the layout and the java file.

--- app/src/main/java/com/example/guest/myrestaurants2/ui/RestaurantsActivity2.java
+++ app/src/main/java/com/example/guest/myrestaurants2/ui/RestaurantsActivity2.java
@@ -29,11 +29,9 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class RestaurantsActivity2 extends AppCompatActivity {
-    @Bind(R.id.locationTextView) TextView mLocationTextView;
-    @Bind(R.id.listView) ListView mListView;
-    private String[] restaurants = new String[] {"HUB", "Garden Bar", "Reel M Inn", "People's Pig", "Rabbits Cafe", "Bar Bar", "Slide Inn", "Nightlight", "The Florida Room", "Fresh Roll", "Eat", "Cruz Room", "Radio Room", "Santeria", "Sauce Box", "Shut Up and Eat", "A Real Taste of India (Foodcart)"};
     public static final String TAG = RestaurantsActivity2.class.getSimpleName();
     public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
     private RestaurantListAdapter mAdapter;
 
@@ -43,26 +41,10 @@ public class RestaurantsActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants2);
         ButterKnife.bind(this);
 
-//        Then, we'll create an ArrayAdapter and set our ListView adapter to the new adapter:
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
-        mListView.setAdapter(adapter);
-
-//        If you click a list item, the name of the item shows in a toast
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(RestaurantsActivity2.this, restaurant, Toast.LENGTH_LONG).show();
-                Log.v(TAG, "In the onItemClickListener");
-            }
-        });
-
 //        Now, let’s pull the data out of the intent extra in our RestaurantsActivity. To do this, we will use the getIntent() method to recreate the Intent,
         Intent intent = getIntent();
 //        and the getStringExtra() method to pull out the location value based using the string key we provided:
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
-        Log.d(TAG, "In the onCreate method");
 
         getRestaurants(location);
     }
