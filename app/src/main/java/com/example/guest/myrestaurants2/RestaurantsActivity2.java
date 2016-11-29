194a39da27846cb69cb027b3f0cb3634c923b25a
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-28 23:31:25 -0800

    run UI thread to retrieve data from API after the app is loaded.

--- app/src/main/java/com/example/guest/myrestaurants2/RestaurantsActivity2.java
+++ app/src/main/java/com/example/guest/myrestaurants2/RestaurantsActivity2.java
@@ -69,14 +69,31 @@ public class RestaurantsActivity2 extends AppCompatActivity {
             }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    String jsonData = response.body().string();
-                    Log.v(TAG, jsonData);
-                    mRestaurants = yelpService.processResults(response);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            public void onResponse(Call call, final Response response) {
+                mRestaurants = yelpService.processResults(response);
+
+                RestaurantsActivity2.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String[] restaurantNames = new String[mRestaurants.size()];
+                        for (int i = 0; i < restaurantNames.length; i++) {
+                            restaurantNames[i] = mRestaurants.get(i).getName();
+                        }
+
+                        ArrayAdapter adapter = new ArrayAdapter(RestaurantsActivity2.this, android.R.layout.simple_list_item_1, restaurantNames);
+                        mListView.setAdapter(adapter);
+
+                        for (Restaurant restaurant : mRestaurants) {
+                            Log.d(TAG, "Name: " +restaurant.getName());Log.d(TAG, "Phone: " + restaurant.getPhone());
+                            Log.d(TAG, "Website: " + restaurant.getWebsite());
+                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
+                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
+                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
+                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
+                        }
+
+                    }
+                });
             }
 
         });
