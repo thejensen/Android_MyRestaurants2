c611a4d2f01980fb78677bf69a4417b970986721
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-28 22:01:48 -0800

    recieve yelp api response in restaurants activity with getRestaurants method.

--- app/src/main/java/com/example/guest/myrestaurants2/RestaurantsActivity2.java
+++ app/src/main/java/com/example/guest/myrestaurants2/RestaurantsActivity2.java
@@ -12,8 +12,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RestaurantsActivity2 extends AppCompatActivity {
     @Bind(R.id.locationTextView) TextView mLocationTextView;
@@ -47,5 +52,29 @@ public class RestaurantsActivity2 extends AppCompatActivity {
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
         Log.d(TAG, "In the onCreate method");
+
+        getRestaurants(location);
+    }
+
+    private void getRestaurants (String location) {
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(location, new Callback() {
+
+            @Override
+            public void onFailure (Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
     }
 }
