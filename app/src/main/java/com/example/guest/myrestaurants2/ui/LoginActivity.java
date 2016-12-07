6a3918aa093d087e15eb646318d471f64d1214e9
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-06 22:20:02 -0800

    let users click the log in link on the sign up screen, navigating them to the login.

--- app/src/main/java/com/example/guest/myrestaurants2/ui/LoginActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants2/ui/LoginActivity.java
@@ -1,15 +1,37 @@
 package com.example.guest.myrestaurants2.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.guest.myrestaurants2.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.registerTextView)
+    TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+//             In the code above, notice we include finish() after creating our Intent. This will destroy the LoginActivity as we depart for the CreateAccountActivity, saving us valuable resources.
+            finish();
+        }
     }
 }
+
