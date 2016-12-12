62f8ad1bcc0db26f1606ec935e2f3f7f34db5141
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-12 11:42:43 -0800

    add simpleItemTouchHelperCallback to end up using as a listener for gestures in the recyclerview, and dl Reorder icon from google materials.

--- app/src/main/java/com/example/guest/myrestaurants2/util/OnStartDragListener.java
+++ app/src/main/java/com/example/guest/myrestaurants2/util/OnStartDragListener.java
@@ -5,6 +5,10 @@ import android.support.v7.widget.RecyclerView;
 /**
  * Created by Guest on 12/12/16.
  */
+
+//We will implement this interface in our SavedRestaurantsListActivity to attach the event listener to our custom ItemTouchHelper which will eventually be attached to our RecyclerView. This interface will pass events back to our adapter allowing us to attach the touch listener to an item in our ViewHolder.
+
 public interface OnStartDragListener {
+//    onStartDrag() will be called when the user begins a 'drag-and-drop' interaction with the touchscreen. viewHolder represents the RecyclerView holder corresponding to the object being dragged.
     void onStartDrag(RecyclerView.ViewHolder viewHolder);
 }
