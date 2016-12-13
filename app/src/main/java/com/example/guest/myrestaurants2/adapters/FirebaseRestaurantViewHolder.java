1b50a3b622cff36af6fa53a9fb0e86651203dbbe
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-12 16:39:14 -0800

    make the firebaseRestaurantViewHolder imageView draggable.

--- app/src/main/java/com/example/guest/myrestaurants2/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/example/guest/myrestaurants2/adapters/FirebaseRestaurantViewHolder.java
@@ -32,6 +32,11 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
 
+//    only the image view is drag enabled, but it takes the rest with it apparently
+//    Considering our ViewHolder contains each of the views in a given item view, it may make sense to do this in the ViewHolder itself. However, the OnStartDragListenerpassed in to our adapter cannot be sent to our ViewHolder with a constructor, since the FirebaseRecyclerAdapter handles the construction of the ViewHolder internally.
+//    Instead, we need to grant our adapter access to the ImageView by declaring it as a public member variable, and then set its OnTouchListener in the adapter. We'll begin this process by tweaking code in our ViewHolder to make the ImageView public:
+    public ImageView mRestaurantImageView;
+
 // We then add member variables to hold the view and context which we set in our constructor.
     View mView;
     Context mContext;
@@ -45,7 +50,7 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
 
 // In our bindRestaurant() method, we first bind the views and then set the image and text views.
     public void bindRestaurant(Restaurant restaurant) {
-        ImageView restaurantImageView = (ImageView) mView.findViewById(R.id.restaurantImageView);
+        mRestaurantImageView = (ImageView) mView.findViewById(R.id.restaurantImageView);
         TextView nameTextView = (TextView) mView.findViewById(R.id.restaurantNameTextView);
         TextView categoryTextView = (TextView) mView.findViewById(R.id.categoryTextView);
         TextView ratingTextView = (TextView) mView.findViewById(R.id.ratingTextView);
@@ -54,7 +59,7 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
                 .load(restaurant.getImageUrl())
                 .resize(MAX_WIDTH, MAX_HEIGHT)
                 .centerCrop()
-                .into(restaurantImageView);
+                .into(mRestaurantImageView);
 
         nameTextView.setText(restaurant.getName());
         categoryTextView.setText(restaurant.getCategories().get(0));
