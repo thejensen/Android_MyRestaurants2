790b971968366756e39706c7bc8027f10461ef3a
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-12 16:34:04 -0800

    We add the ItemTouchHelper as a member variable to that we can use it in the OnStartDragListener's onStartDrag() method.
    In the FirebaseRestaurantListAdapter constructor, this refers to the OnStartDragListener and the Context. Both are necessary to construct an instance of a FirebaseRestaurantListAdapter.
    
    The SimpleItemTouchHelperCallback takes an adapter as a parameter so we pass it the instance of the FirebaseRestaurantListAdapter we just created.
    
    The ItemTouchHelper takes a ItemTouchHelper.Callback as an argument so we can pass it the instance of the SimpleItemTouchHelperCallback that we just created using our adapter.
    
    To enable the interfaces to communicate with the necessary callbacks, we must attach the ItemTouchHelper to our RecyclerView using the attachToRecyclerView() method.
    
    Finally, we call the startDrag() method on the instance of our ItemTouchHelper inside of the onStartDrag() override which will eventually send our touch events back to our SimpleItemTouchHelperCallback.

--- app/src/main/java/com/example/guest/myrestaurants2/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants2/ui/SavedRestaurantListActivity.java
@@ -1,25 +1,31 @@
 package com.example.guest.myrestaurants2.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import com.example.guest.myrestaurants2.Constants;
 import com.example.guest.myrestaurants2.R;
+import com.example.guest.myrestaurants2.adapters.FirebaseRestaurantListAdapter;
 import com.example.guest.myrestaurants2.adapters.FirebaseRestaurantViewHolder;
 import com.example.guest.myrestaurants2.models.Restaurant;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.example.guest.myrestaurants2.util.OnStartDragListener;
+import com.example.guest.myrestaurants2.util.SimpleItemTouchHelperCallback;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SavedRestaurantListActivity extends AppCompatActivity {
+public class SavedRestaurantListActivity extends AppCompatActivity implements OnStartDragListener {
 // First, we initialize our DatabaseReference, FirebaseRecyclerAdapter, and RecyclerView member variables.
     private DatabaseReference mRestaurantReference;
-    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private FirebaseRestaurantListAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
@@ -31,29 +37,37 @@ public class SavedRestaurantListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants2);
         ButterKnife.bind(this);
 
-// Next, we set the mRestaurantReference using the "restaurants" child node key from our Constants class.
-        mRestaurantReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+//// Next, we set the mRestaurantReference using the "restaurants" child node key from our Constants class.
+//        mRestaurantReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+//        setUpFirebaseAdapter();
+
         setUpFirebaseAdapter();
     }
 
 // We then create a method to set up the FirebaseAdapter which takes the model class, the list item layout OF THE DRAGGABLE KIND ;), the view holder, and the database reference as parameters.
 
     private void setUpFirebaseAdapter() {
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Restaurant, FirebaseRestaurantViewHolder>
-                (Restaurant.class, R.layout.restaurant_list_item_drag, FirebaseRestaurantViewHolder.class,
-                        mRestaurantReference) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
 
-// Inside of the populateViewHolder() method, we call the bindRestaurant() method on our viewHolder to set the appropriate text and image with the given restaurant.
-            @Override
-            protected void populateViewHolder(FirebaseRestaurantViewHolder viewHolder,
-                                              Restaurant model, int position) {
-                viewHolder.bindRestaurant(model);
-            }
-        };
+        mRestaurantReference = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS)
+                .child(uid);
+
+        mFirebaseAdapter = new FirebaseRestaurantListAdapter(Restaurant.class,
+                R.layout.restaurant_list_item_drag, FirebaseRestaurantViewHolder.class,
+                mRestaurantReference, this, this);
+
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 // We then set the adapter on our RecyclerView.
         mRecyclerView.setAdapter(mFirebaseAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
+
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
     }
 
 // Finally, we need to clean up the FirebaseAdapter. When the activity is destroyed, we need to call cleanup() on the adapter so that it can stop listening for changes in the Firebase database.
@@ -61,5 +75,10 @@ public class SavedRestaurantListActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mFirebaseAdapter.cleanup();
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
     }
 }
