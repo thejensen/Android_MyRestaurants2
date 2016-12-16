f918ea990c9efcd9f5cb045723c5dfd95b25f38c
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-15 22:25:19 -0800

    inflate animation layouts in the firebaseRestaurantViewHolder.

--- app/src/main/java/com/example/guest/myrestaurants2/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/example/guest/myrestaurants2/adapters/FirebaseRestaurantViewHolder.java
@@ -1,8 +1,9 @@
 package com.example.guest.myrestaurants2.adapters;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,16 +19,16 @@ import com.squareup.picasso.Picasso;
 
 // We also implement the View.OnClickListener interface and set the click listener on our itemView.
 public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
-// Just like in our RestaurantViewHolder, we add static variables to hold the width and height of our images for Picasso.
+    // Just like in our RestaurantViewHolder, we add static variables to hold the width and height of our images for Picasso.
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
 
-//    only the image view is drag enabled, but it takes the rest with it apparently
+    //    only the image view is drag enabled, but it takes the rest with it apparently
 //    Considering our ViewHolder contains each of the views in a given item view, it may make sense to do this in the ViewHolder itself. However, the OnStartDragListenerpassed in to our adapter cannot be sent to our ViewHolder with a constructor, since the FirebaseRecyclerAdapter handles the construction of the ViewHolder internally.
 //    Instead, we need to grant our adapter access to the ImageView by declaring it as a public member variable, and then set its OnTouchListener in the adapter. We'll begin this process by tweaking code in our ViewHolder to make the ImageView public:
     public ImageView mRestaurantImageView;
 
-// We then add member variables to hold the view and context which we set in our constructor.
+    // We then add member variables to hold the view and context which we set in our constructor.
     View mView;
     Context mContext;
 
@@ -37,7 +38,7 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
         mContext = itemView.getContext();
     }
 
-// In our bindRestaurant() method, we first bind the views and then set the image and text views.
+    // In our bindRestaurant() method, we first bind the views and then set the image and text views.
     public void bindRestaurant(Restaurant restaurant) {
         mRestaurantImageView = (ImageView) mView.findViewById(R.id.restaurantImageView);
         TextView nameTextView = (TextView) mView.findViewById(R.id.restaurantNameTextView);
@@ -55,15 +56,20 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
         ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
     }
 
+//    Now that we've created our XML animations, we'll need to inflate them in our ViewHolder. This is similar to the manner we inflate other types of XML layouts, but we'll use a special AnimatorSet object responsible for handling the corresonding <set> tags in our XML animations.
     @Override
     public void onItemSelected() {
-        Log.d("Animation", "onItemSelected");
-
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_on);
+        set.setTarget(itemView);
+        set.start();
     }
 
     @Override
     public void onItemClear() {
-        Log.d("Animation", "onItemClear");
-
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_off);
+        set.setTarget(itemView);
+        set.start();
     }
 }
